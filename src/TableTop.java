4e1b20b9cfd74ee1b958176b45595994587a93a2
Fredrik Billing <frhabi@gmail.com>
2019-10-24 20:04:10 +0200

    Initial push FredrikDevBranch

--- src/TableTop.java
+++ src/TableTop.java
@@ -8,38 +8,28 @@ public class TableTop extends JFrame {
     private JPanel board = new JPanel();
     JButton button = new JButton();
 
+
+
    TableTop(){
 
        add(board);
        setSize(sizeOfBoard);
        setLocationRelativeTo(null);
-       board.setLayout(new GridLayout(4,4));
+       board.setLayout(new GridLayout(2,2));
        for (int i = 1; i < 16; i++) {
            board.add(button = new JButton());
        }
+
+
    }
+
+
+
 
     public static void main(String[] args) {
 
        TableTop spel = new TableTop();
        spel.setVisible(true);
     }
-
-    public static int[][] fillButtonsWithNumbers(){
-
-        int[][] rut = new int[4][4];
-
-        int counter = 1;
-        for(int i = 0; i < rut.length; i++){
-            for(int j = 0; j < rut.length; j++){
-                rut[i][j]=counter;
-                counter++;
-            }
-        }
-        return rut;
-
-
-    }
-
 
 }
