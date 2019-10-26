6f67f7c9f7143170d11506abfddae3e4f873e06e
Fredrik Billing <frhabi@gmail.com>
2019-10-26 12:21:06 +0200

    Bytte till siffror

--- src/Matrix.java
+++ src/Matrix.java
@@ -16,21 +16,21 @@ public class Matrix extends JFrame implements ActionListener {
         setSize(800, 600);
         setLocationRelativeTo(null);
         panel.setLayout(new GridLayout(4, 4));
-
-
     }
-
-
     private void addButtons(JPanel panel) {
+        int counter = 0;
         gameboard = new JButton[4][4];
         for (int y = 0; y < 4; y++) {
             for (int x = 0; x < 4; x++) {
-                gameboard[x][y] = new JButton("");
+                counter++;
+                gameboard[x][y] = new JButton(String.valueOf(counter));
+                gameboard[x][y].setFont(new Font("Arial", Font.PLAIN,40));
                 gameboard[x][y].setBackground(Color.white);
                 gameboard[x][y].addActionListener(this);
                 panel.add(gameboard[x][y]);
             }
         }
+        gameboard[3][3].setText("0");
     }
 
     public static void main(String[] args) {
@@ -45,19 +45,32 @@ public class Matrix extends JFrame implements ActionListener {
                 if (gameboard[y][x] == e.getSource()) {
                     if (sign % 2 == 0) {
                         try {
-                            gameboard[y - 1][x].setBackground(Color.red);
+                            if(gameboard[y - 1][x].getText().equals("0")){
+                                gameboard[y - 1][x].setText(gameboard[y][x].getText());
+                                gameboard[y][x].setText("0");
+                            }
                         } catch (ArrayIndexOutOfBoundsException ignored) {
                         }
                         try {
-                            gameboard[y][x + 1].setBackground(Color.red);
+                            if(gameboard[y][x + 1].getText().equals("0")) {
+                                gameboard[y][x + 1].setText(gameboard[y][x].getText());
+                                gameboard[y][x].setText("0");
+                            }
                         } catch (ArrayIndexOutOfBoundsException ignored) {
                         }
                         try {
-                            gameboard[y + 1][x].setBackground(Color.red);
+                            if(gameboard[y + 1][x].getText().equals("0")){
+                                gameboard[y + 1][x].setText(gameboard[y][x].getText());
+                                gameboard[y][x].setText("0");
+
+                            }
                         } catch (ArrayIndexOutOfBoundsException ignored) {
                         }
                         try {
-                            gameboard[y][x - 1].setBackground(Color.red);
+                            if(gameboard[y][x - 1].getText().equals("0")){
+                                gameboard[y][x - 1].setText(gameboard[y][x].getText());
+                                gameboard[y][x].setText("0");
+                            }
                         } catch (ArrayIndexOutOfBoundsException ignored) {
                         }
                     }
