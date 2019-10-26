b2d075f5379a2ec5e7a84bbdd544f87205844561
Fredrik Billing <frhabi@gmail.com>
2019-10-26 11:46:45 +0200

    All logik

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,86 +1,115 @@
-import java.util.Scanner;
-import java.util.Random;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Matrix {
+    public class Matrix extends JFrame implements ActionListener {
 
-
-    private int[][] matrix;
-
-    /**
-     * Creates an n-by-n symmetric matrix in which all entries are 0.
-     */
-    public Matrix(int n) {
-        matrix = new int[n][];
-        for (int i = 0; i < n; i++)
-            matrix[i] = new int[i + 1];
-    }
-
-    /**
-     * Returns the matrix entry at position (row,col).  (If row < col,
-     * the value is actually stored at position (col,row).)
-     */
-
-    public int get( int row, int col ) {
-        if (row >= col)
-            return matrix[row][col];
-        else
-            return matrix[col][row];
-    }
-
-    /**
-     * Sets the value of the matrix entry at (row,col).  (If row < col,
-     * the value is actually stored at position (col,row).)
-     */
-
-    public void set( int row, int col, int value ) {
-        if (row >= col)
-            matrix[row][col] = value;
-        else
-            matrix[col][row] = value;
-    }
+        JPanel panel = new JPanel();
+        JButton button;
+        private static JButton[][] gameboard = new JButton[4][4];
+        private int sign = 0;
 
 
+        Matrix() {
+            add(panel);
+            addButtons(panel);
+            setVisible(true);
+            setSize(800, 600);
+            setLocationRelativeTo(null);
+            panel.setLayout(new GridLayout(4, 4));
+            int buttonCount = 0;
+            String buttonID;
+            buttonID = Integer.toString(++buttonCount);
 
-    public class Adventure {
+
+        }
+
+
+        private void addButtons(JPanel panel) {
+            gameboard = new JButton[4][4];
+            for (int y = 0; y < 4; y++) {
+                for (int x = 0; x < 4; x++) {
+                    gameboard[x][y] = new JButton("");
+                    gameboard[x][y].setBackground(Color.white);
+                    gameboard[x][y].addActionListener(this);
+                    panel.add(gameboard[x][y]);
+                }
+            }
+        }
+
 
         public static void main(String[] args) {
-            char grid[][]= new char[10][10];
-            Scanner move = new Scanner(System.in);
-            System.out.println("Here is the current game board:");
-            System.out.println("-------------------------------");
 
-            for(int i=0; i<grid.length; i++) {
-                for(int j=0; j<grid.length; j++) {
-                    double random = Math.random();
-                    if(random <=.05) {
-                        grid[i][j]='*';
+            Adventure test = new Adventure();
+
+            gameboard[3][3].setBackground(Color.black);
+
+            int r = 3;
+            int c = 2;
+
+
+        /*try {
+            System.out.println(gameboard[r - 1][c]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("utanför norr");
+        }
+
+        try {
+            System.out.println(gameboard[r][c + 1]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("utanför öst");
+        }
+
+        try {
+            System.out.println(gameboard[r + 1][c]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("utanför söder");
+        }
+
+        try {
+            System.out.println(gameboard[r][c - 1]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("utanför väst");
+        }
+    }*/
+
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            for (int y = 0; y < 4; y++) {
+                for (int x = 0; x < 4; x++) {
+                    if (gameboard[y][x] == e.getSource()) {
+                        if (sign % 2 == 0) {
+                            try {
+                               gameboard[y - 1][x].setBackground(Color.red);
+                            } catch (ArrayIndexOutOfBoundsException ignored) {
+                            }
+                            try {
+                                gameboard[y][x + 1].setBackground(Color.red);
+                            } catch (ArrayIndexOutOfBoundsException ignored) {
+                            }
+                            try {
+                                gameboard[y + 1][x].setBackground(Color.red);
+                            } catch (ArrayIndexOutOfBoundsException ignored) {
+                            }
+                            try {
+                                gameboard[y][x - 1].setBackground(Color.red);
+                            } catch (ArrayIndexOutOfBoundsException ignored) {
+                            }
+                        }
+                        break;
+
                     }
-                    else if(random > .06 && random <= .15) {
-                        grid[i][j]='X';
-                    }
-                    else {
-                        grid[i][j]='.';
-                    }
-                    grid[0][0]='P';
-                    grid[9][9]='T';
-                    System.out.print(grid[i][j]);
+
+
                 }
-                System.out.println("");
+
             }
-            System.out.print("Enter your move (U/D/L/R)>");
+
         }
     }
 
-
-    public int size() {
-        return matrix.length;  // The size is the number of rows.
-    }
-
-    public static void main(String[] args) {
-       Matrix test = new Matrix(6);
-       test.set(1,4,55);
-        System.out.println(test.get(1,4));
-        System.out.println(test.size());
-
-    }
 }
